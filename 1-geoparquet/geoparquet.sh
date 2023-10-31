9f91653afe30de7c3bc165c3e7090b013d11dd7f
Matt Forrest <mforrest@carto.com>
2023-10-31 12:25:16 -0400

    Fixing 1-geoparquet

--- 1-geoparquet/geoparquet.sh
+++ 1-geoparquet/geoparquet.sh
@@ -1,10 +1,14 @@
 docker pull osgeo/gdal
 
-docker run --rm -v //Users:/Users osgeo/gdal \
---network="host" ogrinfo -so -al /Users/mattforrest/Documents/modern-gis-workshop/data/BuildingFootprints.geojson
+docker run --rm -v //Users:/Users/ osgeo/gdal \
+ogrinfo -so -al Users/mattforrest/Desktop/modern-gis-workshop/data/BuildingFootprints.geojson
 
-ogr2ogr -f 'Parquet' data/footprints.parquet data/BuildingFootprints.geojson
+docker run --rm -v //Users:/Users/ osgeo/gdal \
+ogr2ogr -f 'Parquet' Users/mattforrest/Desktop/modern-gis-workshop/data/footprints.parquet \
+Users/mattforrest/Desktop/modern-gis-workshop/data/BuildingFootprints.geojson
 
 # Open Colab notebook for speed comparison https://colab.research.google.com/drive/1tMwNR3j0UDkZPLHd3Dx4g7x2CUkdEJdJ#scrollTo=PUY1U-loaA6u
 
-ogr2ogr -f 'Parquet' data/sea_rise.parquet data/sea_rise.geojson
+docker run --rm -v //Users:/Users/ osgeo/gdal \
+ogr2ogr -f 'Parquet' Users/mattforrest/Desktop/modern-gis-workshop/data/sea_rise.parquet \
+Users/mattforrest/Desktop/modern-gis-workshop/data/sea_rise.geojson
